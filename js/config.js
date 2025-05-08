0e19fd361a7d7f6123699ba14d08d4efe1ce3869
ankitagavade11 <ankitagavade725@gmail.com>
2025-05-08 13:39:22 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_4nH3NWFW4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6gla552bdmphtoneur782pra4m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
